9ebd81905cdbae454f8d98dd1a40661a4c28c5bb
mostafa mosad <mostafa-mosad1>
2024-08-21 12:18:42 +0300

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-git remote add origin https://github.com/mostafa-mosad1/CRUD-System.gitimport { StrictMode } from 'react'
+import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
